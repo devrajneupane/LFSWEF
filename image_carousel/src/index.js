c7c311f50d4cbfe3f043ef98830f8ce9518363ae
devrajneupane <asminneupane79@gmail.com>
2024-06-11 23:36:03 +0545

    fix: update image dynamically
    Update image dynamically, stil rely on `transion` for smooth animation

--- image_carousel/src/index.js
+++ image_carousel/src/index.js
@@ -1,75 +1,46 @@
-var carousel = document.getElementsByClassName("carousel")[0];
-var leftButton = document.getElementsByClassName("leftbtn")[0];
-var rightButton = document.getElementsByClassName("rightbtn")[0];
-var indicators = document.getElementsByClassName("indicator");
-var span = 600;
-var prev = 0;
+var carousel = document.querySelector(".carousel");
+var leftButton = document.getElementById("leftbtn");
+var rightButton = document.getElementById("rightbtn");
+var imgLists = document.querySelectorAll(".slide");
+var indicators = document.querySelectorAll(".indicators div");
 var currentIndex = 0;
-/**
- * Removes the active class from indicator
- */
-var clearActive = function () {
-    for (var i = 0; i < indicators.length; i++) {
-        indicators[i].classList.remove("active");
+var totalImages = imgLists.length - 1;
+// Move to next image after given interval
+var interval = setInterval(function () { return rightButton === null || rightButton === void 0 ? void 0 : rightButton.click(); }, 3000);
+// Move between images
+function executeMove() {
+    carousel.style.left = -imgLists[currentIndex].offsetLeft + "px";
+    var lastActive = document.querySelector("div.active");
+    lastActive === null || lastActive === void 0 ? void 0 : lastActive.classList.remove("active");
+    indicators[currentIndex].classList.add("active");
+    // Reset interval
+    clearInterval(interval);
+    interval = setInterval(function () { return rightButton === null || rightButton === void 0 ? void 0 : rightButton.click(); }, 3000);
+}
+// Go to next image on clicking right button
+rightButton === null || rightButton === void 0 ? void 0 : rightButton.addEventListener("click", function () {
+    if (currentIndex < totalImages) {
+        currentIndex++;
     }
-};
-/**
- * Moves between images with animation
- */
-var executeMove = function (index) {
-    var mov = index * span;
-    carousel.animate([
-        { transform: "translateX(-" + prev + "px)" },
-        { transform: "translateX(-" + mov + "px)" },
-    ], { duration: 500 });
-    // @ts-ignore
-    carousel.style.transform = "translateX(-" + mov + "px)";
-    prev = mov;
-};
-/**
- * Moves between images in given direction
- */
-var moveSlide = function (dir) {
-    if (dir === "left") {
+    else {
+        currentIndex = 0;
+    }
+    executeMove();
+});
+// Go to next image on clicking left button
+leftButton === null || leftButton === void 0 ? void 0 : leftButton.addEventListener("click", function () {
+    if (currentIndex > 0) {
         currentIndex--;
     }
     else {
-        currentIndex++;
+        currentIndex = totalImages;
     }
-    if (currentIndex < 0) {
-        currentIndex = indicators.length - 1;
-    }
-    if (currentIndex > indicators.length - 1) {
-        currentIndex = 0;
-    }
-    executeMove(currentIndex);
-    indicators[currentIndex].classList.add("active");
-    clearInterval(interval);
-    interval = setInterval(function () {
-        rightButton === null || rightButton === void 0 ? void 0 : rightButton.click();
-    }, 3000);
-};
-var _loop_1 = function (i) {
-    indicators[i].addEventListener("click", function () {
-        clearActive();
-        executeMove(i);
-        indicators[i].classList.add("active");
-    });
-};
-/**
- * Add Event listeners to indicators
- */
-for (var i = 0; i < indicators.length; i++) {
-    _loop_1(i);
-}
-leftButton.addEventListener("click", function () {
-    clearActive();
-    moveSlide("left");
+    executeMove();
 });
-var interval = setInterval(function () {
-    rightButton === null || rightButton === void 0 ? void 0 : rightButton.click();
-}, 3000);
-rightButton.addEventListener("click", function () {
-    clearActive();
-    moveSlide("right");
+// Add Event listeners to indicators
+indicators.forEach(function (element, index) {
+    element.addEventListener("click", function () {
+        currentIndex = index;
+        executeMove();
+    });
 });
